0003353e2f683d6c47211bad148d14b12ed8642e
juanelcatl-web <juanelcatl@gmail.com>
2026-02-12 17:46:54 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,10 @@
-const CACHE_NAME = 'amoverse-v2';
-const ASSETS = [
-  './',               // Esto guarda la ruta raíz
-  './index.html',     // Este es el nombre real ahora
-  './manifest.json'
-];
+const CACHE_NAME = 'amoverse-v3';
+const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
