9151521630af0bd041ddd89133c9d026fca006b3
juanelcatl-web <juanelcatl@gmail.com>
2026-02-13 11:38:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,26 @@
-const CACHE_NAME = 'amoverse-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'amoverse-v4-final';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icono.png'
+];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting(); // Fuerza a que la nueva versión tome el mando
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
